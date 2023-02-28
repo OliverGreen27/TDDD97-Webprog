b17235e1d97660da08c57ae265c8d7997b0a57fe
Gustav <gusel725@student.liu.se>
2023-02-28 15:00:37 +0100

    now commiting changes in db

--- lab2/database_helper.py
+++ lab2/database_helper.py
@@ -5,27 +5,28 @@ con = sqlite3.connect("database.db", check_same_thread=False)
 cur = con.cursor()
 
 
+def query_db(query, args=(), fetchone=False, commit=False):
+    res = cur.execute(query, args).fetchall()
+    if commit:
+        con.commit()
+    return (res[0] if res else None) if fetchone else res
+
+
 def get_token(email):
-    res = cur.execute(f"SELECT token FROM logged_in_users WHERE email=?;", (email,))
-    return res.fetchone()
+    return query_db(f"SELECT token FROM logged_in_users WHERE email=?;", (email,), fetchone=True)
 
 
 def get_password(email):
-
-    res = cur.execute("SELECT password_hash FROM user_data WHERE email=?;", (email,))
-    
-    return res.fetchone()
+    return query_db("SELECT password_hash FROM user_data WHERE email=?;", (email,), fetchone=True)
 
 
 def update_logged_in_users(email, token):
-
-    cur.execute("Insert INTO logged_in_users VALUES (?,?);", (email,token))
+    query_db("Insert INTO logged_in_users VALUES (?,?);", (email,token), commit=True)
 
 
 def get_user_data(email):
-    data = cur.execute("SELECT * FROM user_data WHERE email=?;", (email,))
-    return data.fetchall()
+    return query_db("SELECT * FROM user_data WHERE email=?;", (email,))
 
 
 def create_user(email, pw_hash, fname, lname, gender, city, country):
-    cur.execute(f"Insert INTO user_data VALUES (?,?,?,?,?,?,?);", (email, pw_hash, fname, lname, gender, city, country))
+    query_db(f"Insert INTO user_data VALUES (?,?,?,?,?,?,?);", (email, pw_hash, fname, lname, gender, city, country), commit=True)
